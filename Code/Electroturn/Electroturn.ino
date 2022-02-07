cdf42cec79123e9033d5fd399b9e497902eccff4
k4kfh <k4kfh@users.noreply.github.com>
2022-02-06 18:40:04 -0600

    Initial layout ideas, spindle speed computation

--- Code/Electroturn/Electroturn.ino
+++ Code/Electroturn/Electroturn.ino
@@ -11,6 +11,14 @@ QuadEncoder spindleEnc(1, 3, 2, 0);
 // Declare Z axis stepper
 AccelStepper zStepper(AccelStepper::DRIVER, 4, 6);
 
+// Spindle encoder data variables
+int spindleEnc_sample;
+int spindleEnc_sinceLastSpeedComputation;
+
+// Time between each iteration of the main loop
+elapsedMicros microsSinceSample;
+elapsedMicros microsSinceSpeedComputation;
+
 void setup() {
   // Initialize spindle encoder
   spindleEnc.setInitConfig();
@@ -31,11 +39,37 @@ void setup() {
 }
 
 void loop() {
-  int currentSample = spindleEnc.read();
+  // Get encoder movement since last sample, store in variable, immediately write encoder to 0
+  spindleEnc_sample = spindleEnc.read();
+  spindleEnc.write(0);
+  // Add this movement to the movement since the last speed computation
+  spindleEnc_sinceLastSpeedComputation = spindleEnc_sinceLastSpeedComputation + spindleEnc_sample;
+  
+  // Get time elapsed since last sample, store in variable, immediately write time elapsed to 0
+  float microsElapsed_sample = microsSinceSample;
+  microsElapsed_sample = 0;
 
-  // Convert encoder steps to motor movement steps
-  float motorSteps = ((float)currentSample/8000.0)*2000.0;
+  // Speed computation about every .25 seconds
+  if (microsSinceSpeedComputation > 250000) {
+    float spindleRevs = ((float)spindleEnc_sinceLastSpeedComputation/8000.0)*2.0; // the 2.0 is for the pulley ratio
+    float minutes = (float)microsSinceSpeedComputation/6e7;
 
-  zStepper.moveTo(motorSteps);
-  zStepper.run();
+    float spindleRPM = spindleRevs/minutes;
+
+    // Reset the stopwatch and the encoder accumulator
+    microsSinceSpeedComputation = 0;
+    spindleEnc_sinceLastSpeedComputation = 0;
+
+    // Temporarily just print this here
+    Serial.print("Spindle RPM: ");
+    Serial.println(spindleRPM);
+  }
+
+  // If in Z power feed mode:
+  // Check that zStepper.distanceToGo() is within acceptable margin (to make sure stepper isn't lagging way behind encoder)
+  // Compute stepper movement from encoder movement
+  // Call zStepper.moveTo()
+
+  // If in threading mode:
+  // ??
 }
