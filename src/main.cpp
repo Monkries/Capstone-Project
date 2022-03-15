07f45e964236af378c030b9491aefec13c35531c
k4kfh <k4kfh@users.noreply.github.com>
2022-03-15 16:32:27 -0500

    Renaming cfg_ constants to sys_

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,12 @@
 #include <EncoderTach.h> // Our encoder RPM helper library
 
 // System Configuration Info
-const float cfg_encoderPulleyMultiplier = 1.0; // e.g. if the encoder runs at 2X spindle speed, make this 2
-const unsigned int cfg_encoderTicksPerRev = 8000;
-const unsigned int cfg_stepsPerRev = 2000;
-const unsigned int cfg_leadscrewTpi = 20; //1/4-20TPI leadscrew in our case
-const bool cfg_leadscrewIsLeftHand = true; // TODO: integrate this
-const unsigned int cfg_rapidStepRate = 2000;
+const float sys_encoderPulleyMultiplier = 1.0; // e.g. if the encoder runs at 2X spindle speed, make this 2
+const unsigned int sys_encoderTicksPerRev = 8000;
+const unsigned int sys_stepsPerRev = 2000;
+const unsigned int sys_leadscrewTpi = 20; //1/4-20TPI leadscrew in our case
+const bool sys_leadscrewIsLeftHand = true; // TODO: integrate this
+const unsigned int sys_rapidStepRate = 2000;
 
 // Declare spindle encoder
 // Hardware quadrature channel 1, phase A pin 3, phase B pin 2
@@ -20,8 +20,8 @@ QuadEncoder spindleEnc(1, 3, 2, 0);
 AccelStepper zStepper(AccelStepper::DRIVER, 4, 6);
 
 // Tachometer library setup
-// Parameters: 100ms minimum sample interval, 8000 encoder ticks per rev, spindle speed is cfg_encoderPulleyMultiplier*encoder speed
-EncoderTach spindleTach(100000, cfg_encoderTicksPerRev, cfg_encoderPulleyMultiplier);
+// Parameters: 100ms minimum sample interval, 8000 encoder ticks per rev, spindle speed is sys_encoderPulleyMultiplier*encoder speed
+EncoderTach spindleTach(100000, sys_encoderTicksPerRev, sys_encoderPulleyMultiplier);
 
 void setup()
 {
