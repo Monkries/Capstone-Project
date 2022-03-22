bdb8f7750680d5fccc90be68d2278ae3d5bdfbab
Karson Shields <karsonshields1@gmail.com>
2022-03-22 08:24:58 -0500

    Formatted code into new skeleton

--- lib/elsControlPanel/elsControlPanel.cpp
+++ lib/elsControlPanel/elsControlPanel.cpp
@@ -17,10 +17,84 @@ void elsControlPanel::init() {
     rpmReadout.begin(rpmReadouti2cAddress); // Initialize with whatever configured address (default is 0x70, per adafruit)
 }
 
+// button1text controls units, button2text controls hardinge threading, button3text controls nothing currently
 void elsControlPanel::TFT_writeGearboxInfo(String mode, Pitch currentPitch, String button1text, String button2text, String button3text) {
+    Adafruit_ILI9341 tft = Adafruit_ILI9341(10, 14, 11, 13, -1, 12);
+     tft.fillScreen(ILI9341_WHITE);
+     tft.setCursor(0, 0);
+     tft.setTextColor(ILI9341_BLACK);
+     tft.setTextSize(2);
+
+        if (mode == "Power Feed") {
+            Serial.println("Power Feed\n");
+            if (currentPitch.units == tpi) {
+                Serial.print(currentPitch.value);
+                Serial.println("in/REV\n");
+            }
+            else if (currentPitch.units == mm ) {
+                Serial.print(currentPitch.value);
+                Serial.println("mm/REV\n");
+            }
+            Serial.println("mm/in");
+        }
+        else if (mode == "Threading") {
+            if (button2text == "Rapid Right") {
+                Serial.println("Threading\n");
+                if (currentPitch.units == tpi) {
+                    Serial.print(currentPitch.value);  
+                    Serial.println("TPI\n");
+                }
+                else if (currentPitch.units == mm) {
+                    Serial.print(currentPitch.value);
+                    Serial.println("mm\n");
+                }
+                Serial.print("Rapid Right");
+            }
+            else if (button2text == "Rapid Left") {
+                Serial.println("Threading\n");
+                if (currentPitch.units == tpi) {
+                    Serial.print(currentPitch.value);  
+                    Serial.println("TPI\n");
+                }
+                else if (currentPitch.units == mm) {
+                    Serial.print(currentPitch.value);
+                    Serial.println("mm\n");
+                }
+                Serial.print("Rapid Left");
+            }
+            else {
+                Serial.println("Threading\n");
+                if (currentPitch.units == tpi) {
+                    Serial.print(currentPitch.value);  
+                    Serial.println("TPI\n");
+                }
+                else if (currentPitch.units == mm) {
+                    Serial.print(currentPitch.value);
+                    Serial.println("mm\n");
+                }
+                Serial.print("Rapid Off"); 
+            }
+        }
 
 }
 
 void alphanum_writeRPM(unsigned int rpm) {
-    
+    Adafruit_7segment numdisplay = Adafruit_7segment();
+
+    //Write Digit x with number
+    numdisplay.print(250, DEC);
+    numdisplay.writeDisplay();
+    delay(2000);
+    numdisplay.writeDigitNum(0,1);
+    numdisplay.writeDigitNum(1,1);
+    numdisplay.writeDigitNum(2,1);
+    numdisplay.writeDigitNum(3,1);
+    numdisplay.writeDisplay();
+    delay(2000);
+    numdisplay.writeDigitNum(0,5);
+    numdisplay.writeDigitNum(1,2);
+    numdisplay.writeDigitNum(2,9);
+    numdisplay.writeDigitNum(3,6);
+    numdisplay.writeDisplay();
+
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,8 @@
 #include <MultiStepper.h>
 #include <QuadEncoder.h> // Teensy hardware quadrature decoder library
 // "In-House" Libraries
-#include <EncoderTach.h> // Our encoder RPM helper library
-#include <TeensyLeadscrew.h> // Our main "virtual gearbox" backend lib
+#include <EncoderTach.h> // Encoder RPM helper library
+#include <TeensyLeadscrew.h> // Main "virtual gearbox" backend lib
 #include <elsControlPanel.h>
 
 
@@ -21,7 +21,7 @@ LatheHardwareInfo sysSpecs = {
   1.0, // encoderPulleyMultiplier : e.g. if the encoder runs at 2X spindle speed, make this 2
   8000, // encoderTicksPerRev
   2000, // stepsPerRev : usable steps per rev, including microsteps
-  1000000, // maxStepRate : maximum allowable rate for stepper motor (steps per sec)
+  (float)1000000, // maxStepRate : maximum allowable rate for stepper motor (steps per sec)
   {20, tpi, leftHandThread_feedRight}, // leadscrewPitch : a Pitch struct with leadscrew specifications
 };
 
@@ -35,25 +35,8 @@ AccelStepper zStepper(AccelStepper::DRIVER, 4, 6);
 // Backend Electronic Leadscrew "Gearbox" lib setup
 TeensyLeadscrew els(spindleEnc, zStepper, sysSpecs, 100);
 
-// CONTROL PANEL SETUP
-
-// TFT Display Pin Info (3/21/2022)
-// SCK -> 13
-// MISO -> 12
-// MOSI -> 11
-// LCD_CS -> 10
-// SD_CS -> n/c
-// RESET -> 15
-// D/C -> 14
-Adafruit_ILI9341 tftObject(10, 14, 11, 13, 15, 12);
-
-// Alphanumeric Display (for RPM) Pin Info
-// SDA -> 18
-// SCL -> 19
-// We don't actually have to deal with this here, because the class is forced to use the hardware i2c pins
-
-// Create control panel class
-elsControlPanel cPanel(tftObject);
+// Control panel object setup
+elsControlPanel cpanel(&tft, 0x70); // TODO: add pins for everything
 
 void setup()
 {
@@ -64,15 +47,14 @@ void setup()
   spindleEnc.init();
 
   pinMode(4, OUTPUT); // TODO: do we need this?
+
+  // TEMPORARY for TEST LED
+  pinMode(13, OUTPUT);
   
   // Initialize Z stepper
-  zStepper.setMaxSpeed(100000.0);
-  zStepper.setAcceleration(500000.0);
+  zStepper.setMaxSpeed(10000.0);
+  zStepper.setAcceleration(5000.0);
 
-  // Initialize control panel hardware
-  cPanel.init();
-
-  // Initialize electronic leadscrew backend
   els.init();
 
   // TEMPORARY: Configure for test screw, 20tpi, no rapids
@@ -83,6 +65,8 @@ void setup()
 
 void loop()
 {
+  unsigned int rpm = 2156;
   els.cycle();
-  
+  cpanel.alphanum_writeRPM(rpm);
+
 }
