e7b05e8d7c0ec0273870d811fe1885ac3557bf41
Hampton <k4kfh.radio@gmail.com>
2022-04-11 17:01:23 -0500

    More cleaning up in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -46,29 +46,14 @@ TeensyLeadscrew els(spindleEnc, zStepper, sysSpecs, 500);
 // SD_CS -> n/c
 // RESET -> 15
 // D/C -> 14
-
 Adafruit_ILI9341 tftObject(10, 14, 11, 13, 15, 12);
 
 // Control Panel Encoder
 // Hardware quadrature channel 2, phase A on pin 0, phase B on pin 1
 QuadEncoder panelEnc(2, 0, 1, 0);
 
-// Alphanumeric Display (for RPM) Pin Info
-// SDA -> 18
-// SCL -> 19
-// We don't actually have to deal with this here, because the class is forced to use the hardware i2c pins
-
 // Create control panel class
 elsControlPanel cPanel(tftObject, panelEnc);
-
-// // See https://github.com/adafruit/Adafruit-MCP23017-Arduino-Library for table on pin descriptions
-// // #define INTB_PIN 19    // Interrupt pin
-// #define INTA_PIN 20     // Interrupt pin
-// #define BTTN_MODEINC 0  // Mode right scroll pin
-// #define BTTN_MODEDEC 1  // Mode left scroll pin
-// #define BTTN_UNITS 2   // Units toggle pin
-// #define BTTN_RAPID 3   // Rapid scroll pin
-// #define BTTN_X 4       // Future additions pin
 
 void setup()
 {
@@ -89,107 +74,10 @@ void setup()
   // Start the TFT splash screen
   cPanel.TFT_splashscreen();
   delay(2000);
-
-  // I2C chip testing
-  
-  // mcp.begin_I2C();
-  // // MCP pin setup
-  // // Interrupt A pin setup
-  // pinMode(INTA_PIN, INPUT);
-  // mcp.setupInterrupts(false, false, LOW);
-  // mcp.pinMode(BTTN_MODEDEC, INPUT_PULLUP);
-  // mcp.pinMode(BTTN_MODEINC, INPUT_PULLUP);
-  // mcp.pinMode(BTTN_RAPID,INPUT_PULLUP);
-  // mcp.pinMode(BTTN_X, INPUT_PULLUP);
-  // mcp.pinMode(BTTN_UNITS, INPUT_PULLUP);
-  // mcp.setupInterruptPin(BTTN_UNITS, LOW);
-  // mcp.setupInterruptPin(BTTN_MODEDEC, LOW);
-  // mcp.setupInterruptPin(BTTN_MODEINC, LOW);
-  // mcp.setupInterruptPin(BTTN_RAPID, LOW);
-  // mcp.setupInterruptPin(BTTN_X, LOW);
-
-  // debounce.attach(mcp, BTTN_RAPID, 5);
-
-  // Interrupt B pin setup
-  // pinMode(INTB_PIN, INPUT);
 }
 
 void loop()
 {
-  // debounce.update();
-  // Serial.println(units);
-  // /////////////////////////////////////////////////////////////////////////////
-  // //                              I2C Buttons                                //
-  // if (!digitalRead(INTA_PIN)) {
-  //   if (!mcp.digitalRead(BTTN_MODEDEC)) {
-  //     modenum = modenum - 1;      
-  //     if (modenum < 0) {
-  //       modenum = 0;
-  //     }
-  //   }
-  //   if (!mcp.digitalRead(BTTN_MODEINC)) {
-  //     modenum = modenum + 1;
-  //     if (modenum > 1) {
-  //       modenum = 1;
-  //     }
-  //   }
-  //   if (!mcp.digitalRead(BTTN_UNITS)) {
-  //     units++;
-  //     if ((units = 1)) {
-  //       els.gearbox_pitch = {5, tpi, rightHandThread_feedLeft};
-  //     }
-  //     else {
-  //       els.gearbox_pitch = {5, mm, rightHandThread_feedLeft};
-  //     }
-  //     if (units > 2) {
-  //       units = 1;
-  //     }
-  //   }
-  //   if (debounce.fell()) {
-  //     if (!mcp.digitalRead(BTTN_RAPID)) {
-  //       rapidnum = rapidnum + 1;
-  //         if (rapidnum > 2) {
-  //           rapidnum = 0;
-  //         }
-  //     }
-  //   }
-  //   else if (!mcp.digitalRead(BTTN_X)) {
-  //     Serial.println("Hello world");
-  //   }
-  // }
-
-  // // Mode Handling
-  // switch (modenum) {
-  //   case 0: 
-  //     Threading = false;
-  //     PowerFeed = true;
-  //   break;
-  //   case 1: 
-  //     Threading = true;
-  //     PowerFeed = false;
-  //   break;
-  // }
-
-  // // // Rapid Handling
-  // switch (rapidnum) {
-  // case 0:
-  //   els.gearbox_rapidLeft = false;
-  //   els.gearbox_rapidRight = false;
-  // break;
-  // case 1:
-  //   els.gearbox_rapidLeft = true;
-  //   els.gearbox_rapidRight = false;
-  // break;
-  // case 2:
-  //   els.gearbox_rapidLeft = false;
-  //   els.gearbox_rapidRight = true;
-  // break;
-  // }
-  // delay(500);
-  ///////////////////////////////////////////////////////////////////////////////////
-
-
-
   els.cycle();
 
   // RPM display
